b026de6df767a8fd4364b623c1d93a9709338029
Markus Szywon <markus.szywon@shadowjumper.de>
2016-01-22 11:39:14 +0100

    Update: Roundcube to version 1.0.8

--- daemon/DaemonCoreSetup.php
+++ daemon/DaemonCoreSetup.php
@@ -312,7 +312,7 @@ function EasySCP_Directories(){
 function EasySCP_main_configuration_file(){
 	$xml = simplexml_load_file(DaemonConfig::$cfg->{'ROOT_DIR'} . '/../setup/config.xml');
 
-	DaemonConfig::$cfg->{'BuildDate'} = '20160119';
+	DaemonConfig::$cfg->{'BuildDate'} = '20160122';
 	DaemonConfig::$cfg->{'DistName'} = $xml->{'DistName'};
 	DaemonConfig::$cfg->{'DistVersion'} = $xml->{'DistVersion'};
 	DaemonConfig::$cfg->{'DEFAULT_ADMIN_ADDRESS'} = $xml->{'PANEL_MAIL'};
